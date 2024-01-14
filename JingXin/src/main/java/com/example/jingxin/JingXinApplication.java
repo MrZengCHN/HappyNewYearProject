1065baf482665ac5973709de3cc5ead9e5143b73
MrZengCHN <mrzeng1164334031@gmail.com>
2024-01-14 23:44:27 +0800

    创建SpringBoot

---
+++ JingXin/src/main/java/com/example/jingxin/JingXinApplication.java
@@ -0,0 +1,13 @@
+package com.example.jingxin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingXinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingXinApplication.class, args);
+    }
+
+}
