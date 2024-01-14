0f40062d5663321841f020d19cb3e4c151f8d9b4
MrZengCHN <mrzeng1164334031@gmail.com>
2024-01-15 01:29:04 +0800

    登录页面完成

---
+++ JingXin/src/main/java/com/example/jingxin/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.example.jingxin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String Login() {
+        return "login";
+    }
+}
